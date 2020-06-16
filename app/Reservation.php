7e05fdad86a3c80433f956ffe9652f1304d81a51
rodzy <49137701+rodzy@users.noreply.github.com>
2020-06-16 14:00:25 -0600

    Users relations

--- app/Reservation.php
+++ app/Reservation.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
-    //
+    /**
+     * Relations
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'foreign_key', 'other_key');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Relations
+     */
+    public function role()
+    {
+        return $this->belongsTo('App\Role', 'foreign_key', 'other_key');
+    }
+    public function reservations()
+    {
+        return $this->hasMany('App\Reservation', 'foreign_key', 'local_key');
+    }
 }
