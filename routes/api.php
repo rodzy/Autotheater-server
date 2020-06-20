883e7a176c275773d54e9a9ed4c8826bf27b98c3
rodzy <49137701+rodzy@users.noreply.github.com>
2020-06-20 12:41:14 -0600

    Auth & movies routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::group(['prefix' => 'movies'], function () {
+        Route::group(['prefix' => 'auth'], function ($router) {
+            Route::post('register', 'AuthController@register');
+            Route::post('login', 'AuthController@login');
+            Route::post('logout', 'AuthController@logout');
+            Route::post('refresh', 'AuthController@refresh');
+            Route::post('me', 'AuthController@me');
+        });
+        Route::get('', 'MovieController@index');
+        Route::get('all', 'MovieController@all');
+        Route::get('/{id}', 'MovieController@show');
+    });
+});
